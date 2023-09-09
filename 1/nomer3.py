5963bde716bc676c1095b811c4c6d0792099be14
nikit1ch <144485663+nikit1ch@users.noreply.github.com>
2023-09-09 21:14:47 +0300

    Add files via upload

---
+++ 1/nomer3.py
@@ -0,0 +1,6 @@
+import math
+X=float(input("X:"))
+Y=float(input("Y:"))
+Z=float(input("Z:"))
+lvec=math.sqrt(X*X+Y*Y+Z*Z)
+print('Длина вектора =',lvec)
---
+++ 1/nomer4.py
@@ -0,0 +1,6 @@
+palindrom=str(input())
+m=palindrom[::-1]
+if palindrom == m:
+  print("являеется палиндромом")
+else:
+  print("не являеется палиндромом")
