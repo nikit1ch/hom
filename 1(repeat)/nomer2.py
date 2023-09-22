e84d8ad012b9ecfc088bb347b095988799d2eb1f
nikit1ch <144485663+nikit1ch@users.noreply.github.com>
2023-09-23 00:21:36 +0300

    Add files via upload

---
+++ 1(repeat)/nomer2.py
@@ -0,0 +1,10 @@
+a = int(input("a:"))
+b = int(input("b:"))
+c = int(input("c:"))
+d = int(input("d:"))
+f = int(input("f:"))
+if f - d != 0:
+ m=(a * b - c) / (f - d)
+ print(m)
+else:
+ print("Делить на ноль нельзя!")
