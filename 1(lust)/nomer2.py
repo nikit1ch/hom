e1c7cd16f6f3a9142585b779535572c6e1d55800
nikit1ch <144485663+nikit1ch@users.noreply.github.com>
2023-09-23 14:43:23 +0300

    Add files via upload

---
+++ 1(lust)/nomer2.py
@@ -0,0 +1,10 @@
+a = float(input("a:"))
+b = float(input("b:"))
+c = float(input("c:"))
+d = float(input("d:"))
+f = float(input("f:"))
+if f - d != 0:
+    m = (a * b - c) / (f - d)
+    print(m)
+else:
+    print("Делить на ноль нельзя!")
