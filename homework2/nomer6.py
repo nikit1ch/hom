8fa024cf65d313b03fcd31f34ec228ecf8e3ac72
nikit1ch <144485663+nikit1ch@users.noreply.github.com>
2023-10-07 14:22:01 +0300

    Update nomer6.py

--- homework2/nomer6.py
+++ homework2/nomer6.py
@@ -1,10 +1,11 @@
-a = input()
-n = int(input()) - 1
-ints = []
-for c in a:
-    try:
-        b = int(c)
-        ints.append(b)
-    except:
-        pass
-print(f"{n+1}-ая цифра в строке {ints[n]}")
+list = []
+y = input("введите число ")
+while y:
+    y = input("введите число ")
+    list.append(y)
+    if len(list) > 100:
+        break
+    elif y ==():
+        break
+k = int(input("введите номер числа "))
+print(list [k])
