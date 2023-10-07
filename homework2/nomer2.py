bbbaea1b7575876dbf6d77dfa47ce7d78311de2d
nikit1ch <144485663+nikit1ch@users.noreply.github.com>
2023-10-07 14:21:28 +0300

    Update nomer2.py

--- homework2/nomer2.py
+++ homework2/nomer2.py
@@ -1,10 +1,7 @@
-spisok = []
-while True:
-    a = input ('Введите число')
-    if a == '':
-        break
-    else:
-        spisok.append(int(a))
-spisok.sort(Key = lambda x: int(str(x)[0]), reverse = True)
-for i in range( 0 , len(spisok)):
-    print(spisok[i], end = ' ')
+list = []
+y = input("введите число ")
+while y:
+    y = input("введите число ")
+    list.append(y)
+    list.sort(reverse=True)
+print("".join(list))
